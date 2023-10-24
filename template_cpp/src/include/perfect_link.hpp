2503730d9b2f9cb7d554e0dbed7a066e68b86a17
Andrija Jelenkovic <andrija.jelenkovic@protonmail.com>
2023-10-24 16:20:02 +0200

    Change the output location when sending

--- template_cpp/src/include/perfect_link.hpp
+++ template_cpp/src/include/perfect_link.hpp
@@ -14,7 +14,8 @@
 #include <string>
 #include <thread>
 
-class PerfectLink {
+class PerfectLink
+{
 private:
     ConcurrentQueue<TransportMessage> q;
     FairLossLink link;
@@ -22,7 +23,6 @@ private:
     HostLookup host_lookup;
     AckSet acked_messages;
     DeliverySet delivered_messages;
-    std::set<uint64_t> sent_messages;
     std::shared_ptr<OutputFile> output_file;
     // !Check if writing of send should be when we actually do a UDP send, or when we trigger the
     // !PerfectLink send command.
@@ -30,57 +30,58 @@ private:
 public:
     PerfectLink(Address address, HostLookup host_lookup, std::shared_ptr<OutputFile> output_file)
         : link(address), host_lookup(host_lookup), acked_messages(host_lookup),
-          delivered_messages(host_lookup), output_file(output_file) {
+          delivered_messages(host_lookup), output_file(output_file)
+    {
         std::cout << "Initialized Perfect link on address: " << address.to_string() << std::endl;
     }
 
-    void send(Address address, Message &message) {
+    void send(Address address, Message &message)
+    {
         // std::cout << "Triggered Perfect link send on address: " << address.to_string() <<
         // std::endl;
-
+        this->output_file.get()->write("b " + std::to_string(message.get_id()) + "\n");
         uint64_t length = 0;
         auto payload = message.serialize(length);
         q.push(TransportMessage(address, message.get_id(), payload, length));
     }
 
-    void shut_down() {
+    void shut_down()
+    {
         this->link.stop_receiving();
         this->stop_sending();
     }
 
     Address get_address() { return this->link.address; }
 
-    std::thread start_sending() {
+    std::thread start_sending()
+    {
         std::cout << "Started sending on: " << this->link.address.to_string() << std::endl;
 
-        return std::thread([this]() {
+        return std::thread([this]()
+                           {
             while (this->continue_sending) {
                 auto first = this->q.dequeue();
                 if (first.has_value()) {
                     TransportMessage m = first.value();
 
-                    if (this->sent_messages.find(m.get_id()) == this->sent_messages.cend()) {
-                        this->output_file.get()->write("b " + std::to_string(m.get_id()) + "\n");
-                    }
-
                     if (!this->acked_messages.is_acked(m)) {
                         this->link.send(m);
                         this->q.push(m);
-                        this->sent_messages.insert(m.get_id());
                     }
                 }
             }
 
             std::cout << "Shut down sending on node: " << this->link.address.to_string()
-                      << std::endl;
-        });
+                      << std::endl; });
     }
 
-    std::thread start_receiving(std::function<void(TransportMessage)> handler) {
+    std::thread start_receiving(std::function<void(TransportMessage)> handler)
+    {
         std::cout << "Started receiving on: " << this->link.address.to_string() << std::endl;
 
-        return std::thread([this, handler]() {
-            this->link.start_receiving([this, handler](TransportMessage message) {
+        return std::thread([this, handler]()
+                           { this->link.start_receiving([this, handler](TransportMessage message)
+                                                        {
                 if (message.is_ack) {
                     // std::cout << "Received ack for: " << message.get_id()
                     //           << "from: " << message.address.to_string() << std::endl;
@@ -102,13 +103,12 @@ public:
                         "d " + std::to_string(host_lookup.get_host_id_by_ip(message.address)) +
                         " " + std::to_string(message.get_id()) + "\n");
                     handler(message);
-                }
-            });
-        });
+                } }); });
     }
 
 private:
-    void stop_sending() {
+    void stop_sending()
+    {
         this->continue_sending = false;
         std::cout << "Stopping sending on node: " << this->link.address.to_string() << std::endl;
     }
