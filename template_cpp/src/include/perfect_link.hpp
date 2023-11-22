4e55412f6067135dcee4babfc0df788f754f57dc
Andrija Jelenkovic <andrija.jelenkovic@protonmail.com>
2023-11-22 11:39:18 +0100

    format perfect_link.hpp and transport_message.hpp

--- template_cpp/src/include/perfect_link.hpp
+++ template_cpp/src/include/perfect_link.hpp
@@ -14,8 +14,7 @@
 #include <string>
 #include <thread>
 
-class PerfectLink
-{
+class PerfectLink {
 private:
     ConcurrentQueue<TransportMessage> q;
     FairLossLink link;
@@ -24,44 +23,45 @@ private:
     AckSet acked_messages;
     DeliverySet delivered_messages;
     std::shared_ptr<OutputFile> output_file;
-    // !Check if writing of send should be when we actually do a UDP send, or when we trigger the
-    // !PerfectLink send command.
+    // !Check if writing of send should be when we actually do a UDP send, or
+    // when we trigger the !PerfectLink send command.
 
 public:
-    PerfectLink(Address address, HostLookup host_lookup, std::shared_ptr<OutputFile> output_file)
-        : link(address, host_lookup), host_lookup(host_lookup), acked_messages(host_lookup), delivered_messages(host_lookup), output_file(output_file)
-    {
-        std::cout << "Initialized Perfect link on address: " << address.to_string() << std::endl;
+    PerfectLink(Address address, HostLookup host_lookup,
+                std::shared_ptr<OutputFile> output_file)
+        : link(address, host_lookup), host_lookup(host_lookup),
+          acked_messages(host_lookup), delivered_messages(host_lookup),
+          output_file(output_file) {
+        std::cout << "Initialized Perfect link on address: "
+                  << address.to_string() << std::endl;
     }
 
-    void send(Address address, Message &message)
-    {
-        // std::cout << "Triggered Perfect link send on address: " << address.to_string() <<
-        // std::endl;
-        this->output_file.get()->write("b " + std::to_string(message.get_id()) + "\n");
+    void send(Address address, Message &message) {
+        // std::cout << "Triggered Perfect link send on address: " <<
+        // address.to_string() << std::endl;
+        this->output_file.get()->write("b " + std::to_string(message.get_id()) +
+                                       "\n");
         uint64_t length = 0;
         auto payload = message.serialize(length);
 
-        while (q.is_full())
-        {
+        while (q.is_full()) {
         }
-        q.push(TransportMessage(address, message.get_id(), std::move(payload), length));
+        q.push(TransportMessage(address, message.get_id(), std::move(payload),
+                                length));
     }
 
-    void shut_down()
-    {
+    void shut_down() {
         this->link.stop_receiving();
         this->stop_sending();
     }
 
     Address get_address() { return this->link.address; }
 
-    std::thread start_sending()
-    {
-        std::cout << "Started sending on: " << this->link.address.to_string() << std::endl;
+    std::thread start_sending() {
+        std::cout << "Started sending on: " << this->link.address.to_string()
+                  << std::endl;
 
-        return std::thread([this]()
-                           {
+        return std::thread([this]() {
             while (true) {
                 auto first = this->q.dequeue();
                 if (first.has_value()) {
@@ -77,45 +77,53 @@ public:
                 }
             }
 
-            std::cout << "Shut down sending on node: " << this->link.address.to_string()
-                      << std::endl; });
+            std::cout << "Shut down sending on node: "
+                      << this->link.address.to_string() << std::endl;
+        });
     }
 
-    std::thread start_receiving(std::function<void(TransportMessage)> handler)
-    {
-        std::cout << "Started receiving on: " << this->link.address.to_string() << std::endl;
+    std::thread start_receiving(std::function<void(TransportMessage)> handler) {
+        std::cout << "Started receiving on: " << this->link.address.to_string()
+                  << std::endl;
 
-        return std::thread([this, handler]()
-                           { this->link.start_receiving([this, handler](TransportMessage message)
-                                                        {
+        return std::thread([this, handler]() {
+            this->link.start_receiving([this,
+                                        handler](TransportMessage message) {
                 if (message.is_ack) {
                     // std::cout << "Received ack for: " << message.get_id()
-                    //           << "from: " << message.address.to_string() << std::endl;
+                    //           << "from: " << message.address.to_string() <<
+                    //           std::endl;
                     this->acked_messages.ack(message);
 
                     return;
                 }
 
                 // std::cout << "Received message: " << message.get_id()
-                //           << " from: " << message.address.to_string() << std::endl;
+                //           << " from: " << message.address.to_string() <<
+                //           std::endl;
                 if (!this->delivered_messages.is_delivered(message)) {
                     this->delivered_messages.deliver(message);
-                    this->link.send(TransportMessage(message.address, message.get_id(),
-                                                     std::shared_ptr<char[]>(new char[0]), 0,
-                                                     true));
-                    // std::cout << "Delivering the message: " << message.get_id() << std::endl;
+                    this->link.send(TransportMessage(
+                        message.address, message.get_id(),
+                        std::shared_ptr<char[]>(new char[0]), 0, true));
+                    // std::cout << "Delivering the message: " <<
+                    // message.get_id() << std::endl;
 
                     this->output_file.get()->write(
-                        "d " + std::to_string(static_cast<int>(host_lookup.get_host_id_by_ip(message.address))) +
+                        "d " +
+                        std::to_string(static_cast<int>(
+                            host_lookup.get_host_id_by_ip(message.address))) +
                         " " + std::to_string(message.get_id()) + "\n");
                     handler(message);
-                } }); });
+                }
+            });
+        });
     }
 
 private:
-    void stop_sending()
-    {
+    void stop_sending() {
         this->continue_sending = false;
-        std::cout << "Stopping sending on node: " << this->link.address.to_string() << std::endl;
+        std::cout << "Stopping sending on node: "
+                  << this->link.address.to_string() << std::endl;
     }
 };
--- template_cpp/src/include/transport_message.hpp
+++ template_cpp/src/include/transport_message.hpp
@@ -8,8 +8,7 @@
 #include <memory>
 #include <string>
 
-class TransportMessage
-{
+class TransportMessage {
 private:
     uint32_t id;
     std::shared_ptr<char[]> payload;
@@ -20,32 +19,36 @@ public:
     bool is_ack;
 
     // constructor for creating transport message from message
-    TransportMessage(Address address, const uint32_t id, std::shared_ptr<char[]> payload,
-                     const uint64_t length, bool is_ack = false)
-        : id(id), payload(payload), address(address), length(length), is_ack(is_ack)
-    {
+    TransportMessage(Address address, const uint32_t id,
+                     std::shared_ptr<char[]> payload, const uint64_t length,
+                     bool is_ack = false)
+        : id(id), payload(payload), address(address), length(length),
+          is_ack(is_ack) {
         // std::memcpy(payload.get(), bytes, length);
     }
 
     // constructor for deserializing a message
     TransportMessage(Address address, const char *bytes, uint64_t length)
-        : id(0), payload(new char[length - sizeof(id) - sizeof(is_ack)]), address(address),
-          length(length - sizeof(id) - sizeof(is_ack))
-    {
+        : id(0), payload(new char[length - sizeof(id) - sizeof(is_ack)]),
+          address(address), length(length - sizeof(id) - sizeof(is_ack)) {
         std::memcpy(&this->id, bytes, sizeof(this->id));
-        std::memcpy(&this->is_ack, bytes + sizeof(this->id), sizeof(this->is_ack));
-        std::memcpy(payload.get(), bytes + sizeof(this->id) + sizeof(this->is_ack),
+        std::memcpy(&this->is_ack, bytes + sizeof(this->id),
+                    sizeof(this->is_ack));
+        std::memcpy(payload.get(),
+                    bytes + sizeof(this->id) + sizeof(this->is_ack),
                     length - sizeof(this->id) - sizeof(this->is_ack));
     }
 
-    std::unique_ptr<char[]> serialize(uint64_t &serialized_length)
-    {
-        serialized_length = this->length + sizeof(this->id) + sizeof(this->is_ack);
-        std::unique_ptr<char[]> bytes = std::unique_ptr<char[]>(new char[serialized_length]);
+    std::unique_ptr<char[]> serialize(uint64_t &serialized_length) {
+        serialized_length =
+            this->length + sizeof(this->id) + sizeof(this->is_ack);
+        std::unique_ptr<char[]> bytes =
+            std::unique_ptr<char[]>(new char[serialized_length]);
         std::memcpy(bytes.get(), &this->id, sizeof(this->id));
-        std::memcpy(bytes.get() + sizeof(this->id), &this->is_ack, sizeof(this->is_ack));
-        std::memcpy(bytes.get() + sizeof(this->id) + sizeof(this->is_ack), this->payload.get(),
-                    this->length);
+        std::memcpy(bytes.get() + sizeof(this->id), &this->is_ack,
+                    sizeof(this->is_ack));
+        std::memcpy(bytes.get() + sizeof(this->id) + sizeof(this->is_ack),
+                    this->payload.get(), this->length);
 
         return std::move(bytes);
     }
