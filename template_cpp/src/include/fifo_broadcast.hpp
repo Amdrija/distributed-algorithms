faf3ff202a4b631e47e613576ed61afe60be400c
Andrija Jelenkovic <andrija.jelenkovic@protonmail.com>
2023-11-25 00:03:53 +0100

    Make a working fifo example

--- template_cpp/src/include/fifo_broadcast.hpp
+++ template_cpp/src/include/fifo_broadcast.hpp
@@ -2,34 +2,58 @@
 
 #include "sorted_list.hpp"
 #include "uniform_reliable_broadcast.hpp"
+#include <atomic>
 
+// TODO: Improve the atrocious performance
 class FifoBroadcast {
     UniformReliableBroadcast urb;
-    std::unique_ptr<uint32_t[]> last_delivered;
+    std::unique_ptr<std::atomic_uint32_t[]> last_delivered;
     std::unique_ptr<SortedList[]> pending_messages;
+    std::function<void(BroadcastMessage)> handler;
 
 public:
     // the +1 is because the hosts start from 1
     FifoBroadcast(uint8_t host_id, HostLookup lookup,
                   std::function<void(BroadcastMessage)> handler)
         : urb(host_id, lookup,
-              [this, handler](BroadcastMessage message) {
-                  if (this->last_delivered[message.get_source()] + 1 ==
-                      message.get_sequence_number()) {
-                      this->last_delivered[message.get_source()]++;
-                      handler(std::move(message));
-                      while (true) {
-                      } // Here process the messages in order
-                  } else {
-                      this->pending_messages.get()->insert(std::move(message));
-                  }
+              [this](BroadcastMessage message) {
+                  this->handle_message(std::move(message));
               }),
-          last_delivered(new uint32_t[lookup.get_host_count() + 1]),
-          pending_messages(new SortedList[lookup.get_host_count() + 1]) {}
+          last_delivered(new std::atomic_uint32_t[lookup.get_host_count() + 1]),
+          pending_messages(new SortedList[lookup.get_host_count() + 1]),
+          handler(handler) {
+        for (auto host : lookup.get_hosts()) {
+            this->last_delivered[host] = 0;
+        }
+    }
 
     void broadcast(Message &m, uint32_t sequence_number) {
         this->urb.broadcast(m, sequence_number);
     }
 
     void shut_down() { this->urb.shut_down(); }
+
+private:
+    void handle_message(BroadcastMessage message) {
+        uint8_t host = message.get_source();
+        if (host == 1) {
+            std::cout << "Andrija " << this->last_delivered.get()[host]
+                      << std::endl;
+        }
+        if (this->last_delivered[host] + 1 == message.get_sequence_number()) {
+            this->last_delivered[host]++;
+            this->handler(std::move(message));
+
+            // TODO: Possible optimization to process in batches
+            while (!this->pending_messages.get()[host].empty() &&
+                   this->last_delivered[host] + 1 ==
+                       this->pending_messages.get()[host]
+                           .lowest_sequence_number()) {
+                this->last_delivered[host]++;
+                handler(this->pending_messages.get()[host].pop_front());
+            } // Here process the messages in order
+        } else {
+            this->pending_messages.get()->insert(std::move(message));
+        }
+    }
 };
--- template_cpp/src/include/sorted_list.hpp
+++ template_cpp/src/include/sorted_list.hpp
@@ -2,32 +2,49 @@
 
 #include "message.hpp"
 #include <list>
+#include <mutex>
 #include <optional>
 
+// It has to be concurrent because it will be accessed from 2 threads
 class SortedList {
     std::list<BroadcastMessage> list;
+    std::mutex lock;
 
 public:
-    void insert(BroadcastMessage m) {
+    void insert(BroadcastMessage message) {
+        this->lock.lock();
         auto begin = list.begin();
 
         while (begin != list.end() &&
-               m.get_sequence_number() > begin->get_sequence_number()) {
+               message.get_sequence_number() > begin->get_sequence_number()) {
             begin++;
         }
 
-        list.insert(begin, m);
+        list.insert(begin, std::move(message));
+        this->lock.unlock();
     }
 
-    std::optional<BroadcastMessage &> front() {
-        return list.empty()
-                   ? std::nullopt
-                   : std::make_optional<BroadcastMessage &>(list.front());
+    uint32_t lowest_sequence_number() {
+        this->lock.lock();
+        auto result = list.front().get_sequence_number();
+        this->lock.unlock();
+
+        return result;
+    }
+
+    bool empty() {
+        this->lock.lock();
+        bool empty = list.empty();
+        this->lock.unlock();
+
+        return empty;
     }
 
     BroadcastMessage pop_front() {
+        this->lock.lock();
         BroadcastMessage bm = std::move(list.front());
         list.pop_front();
+        this->lock.unlock();
 
         return bm;
     }
